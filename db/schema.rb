5dd0f3b9f5bee94e89bd0ab6291b9c0947864cad
adam-conway <adam.n.conway@gmail.com>
2018-07-27 10:40:10 -0600

    working on adding comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_27_161034) do
+ActiveRecord::Schema.define(version: 2018_07_27_163358) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.string "content"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
