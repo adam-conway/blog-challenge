e6038b086dcd1a3900b5a1e1bf60aaadd4077833
adam-conway <adam.n.conway@gmail.com>
2018-07-27 10:48:35 -0600

    Have comments showing on post page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,11 +24,12 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
+    post = Post.find(params["post_id"])
     @comment = Comment.new(comment_params)
-
+    @comment.post_id = post.id
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to post_path(post), notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
